d2f041edb7265edd7c6f26917dc9ecdf525cfc01
koji <kou747tanaka@gmail.com>
2022-09-23 23:36:39 +0900

    [A]Register post method was added on the server side.

--- express/server.js
+++ express/server.js
@@ -11,6 +11,34 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.send("Here is express");
+})
+
+mongoose.connect("mongodb://localhost:27017/videoUserDB", {
+    useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        password: md5(req.body.password)
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("congrats! new user is added.")
+        }
+    });
+})
 
 app.listen(3000, function () {
     console.log(`Server is running on port ${PORT}`);
